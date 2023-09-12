a50b0e3e8882906dab36b8b981ac6af8b819cb88
Zair_Hanzo <zairganiev2858@gmail.com>
2023-09-12 18:00:15 +0300

    Nothing at all!

--- settings.py
+++ settings.py
@@ -7,5 +7,5 @@ class Settings:
         self.screen_width = 1920
         self.screen_height = 1080
         self.bg_color = (100, 200, 180)
-        self.ship_speed = 1.5
+        self.ship_speed = 1.5       # 2.61 sec for speed 1, 1.82 sec for speed 1.5, 1.31 sec for speed 2
         
--- ship.py
+++ ship.py
@@ -21,24 +21,34 @@ class Ship:
         # self.y = 700
         # self.rect = self.image.get_rect(x=self.x, y=self.y)
 
-        # Store a decimal value for the ship's horizontal position.
-        self.x = float(self.rect.x)
+        # # Store a decimal value for the ship's horizontal position.
+        # self.x = float(self.rect.x)
 
-        # Movement flag
+    #     # Movement flag
         self.moving_right = False
         self.moving_left = False
 
-    def update(self):
-        """ Update the ship's position based on the movement flags. """
-        """ Update the ship's x value, not the rect """
-        if self.moving_right:
-            self.x += self.settings.ship_speed
-        if self.moving_left:
-            self.x -= self.settings.ship_speed
+    # def update(self):
+    #     """ Update the ship's position based on the movement flags. """
+    #     """ Update the ship's x value, not the rect """
+    #     if self.moving_right:
+    #         self.x += self.settings.ship_speed
+    #     if self.moving_left:
+    #         self.x -= self.settings.ship_speed
 
-        # Update rect object from self x
-        self.rect.x = self.x
-    
+    #     # Update rect object from self x
+    #     self.rect.x = self.x
+
+
+# With this modification for 'moving left' flag rect.x attribute decreases in 1(instead of 1.5) and increases on 2(instead of 1.5)
+
+    # def update(self):
+    #     if self.moving_right:
+    #     #     self.rect.x += self.settings.ship_speed
+    #     # if self.moving_left:
+    #     #     # self.rect.x -= self.settings.ship_speed
+
+
     def blitme(self):
         """ Draw the ship at its current location. """
         self.screen.blit(self.image, self.rect)
