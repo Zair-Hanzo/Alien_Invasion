bf653cb7fcba79ced87660811fee52a93bab9ad9
Zair_Hanzo <zairganiev2858@gmail.com>
2023-10-10 12:31:42 +0300

    Aliens attacking

--- alien_invasion.py
+++ alien_invasion.py
@@ -36,11 +36,13 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
-            """ Respond to keypresses and mouse events """
-            for event in pygame.event.get():
+        """ Respond to keypresses and mouse events """
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
@@ -69,7 +71,7 @@ class AlienInvasion:
                   self.ship.moving_right = False
             elif event.key == pygame.K_LEFT:
                  self.ship.moving_left = False
-        
+         
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group """
         if len(self.bullets) < self.settings.bullets_allowed:
@@ -78,6 +80,7 @@ class AlienInvasion:
 
     def _update_bullets(self):
         """Update position of bullets and get rid of old bullets."""
+    
         # Update bullet positions.
         self.bullets.update()
         """ Get rid of bullets that have disappeared. """
@@ -85,6 +88,21 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         # print(len(self.bullets))
+
+        """ Check for any bullets that have hit aliens """
+        """ If so get rid of the bullet and the alien """
+        collisions = pygame.sprite.groupcollide(
+             self.bullets, self.aliens, False, True)
+    
+    def _update_aliens(self):
+        """ Update the position of all aliens in the fleet """
+        """ 
+        Check if the fleet is at an edge 
+        then update the positions of all aliens in the fleet
+          """
+        self._check_fleet_edges()
+        self.aliens.update()
+         
 
     def _create_fleet(self):
         """ Create the fleet of aliens """
@@ -98,22 +116,39 @@ class AlienInvasion:
         # Determine the number of rows of aliens that fit on the screen.
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_height -
-                                    (3 * alien_height) - ship_height)
+                                    (13 * alien_height) - ship_height)
         number_rows = available_space_y // (2 * alien_height)
 
-        # Create the first row of aliens.
-        
-        for alien_number in range(number_aliens_x):
-             self._create_alien(alien_number)
+        # Create the full fleet of aliens.
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
             #  Create an alien and place it in the row
             alien = Alien(self)
-            alien_width = alien.rect.width
+            alien_width, alien_height = alien.rect.size
             alien.x = alien_width + 2 * alien_width * alien_number
             alien.rect.x = alien.x
+            alien.y = alien_height + 2 * alien_height * row_number
+            alien.rect.y = alien.y
             self.aliens.add(alien)
 
+    def _check_fleet_edges(self):
+        """ Respond appropriately if any aliens have reached an edge """
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+        """ Drop the entire fleet and change the fleet direction """
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+            
+                
     def _update_screen(self):
             """ Update images on the screen, and flip to the new screen """
             self.screen.fill(self.settings.bg_color)
@@ -128,7 +163,6 @@ if __name__ == '__main__':
     # Make a game instance and run the game 
     ai = AlienInvasion()
     ai.run_game()
-
 
 
 
